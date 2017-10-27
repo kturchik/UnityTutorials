c1f29028836ab5818d4f475f5da90fdc78ae8130
klturchik <kturchik@sbcglobal.net>
2017-10-27 10:21:27 -0700

    Added PongGame, TronGame, and GroundhogGame

---
+++ 2-TronGame/TronGame/Assets/Move.cs
@@ -0,0 +1,87 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Move : MonoBehaviour {	
+    // Movement keys (customizable in Inspector)
+    public KeyCode upKey;
+    public KeyCode downKey;
+    public KeyCode rightKey;
+    public KeyCode leftKey;
+
+    private string currentDirection = "up";
+    
+    // Movement Speed
+    public float speed = 16;
+    
+    // Wall Prefab
+    public GameObject wallPrefab;
+    
+    // Current Wall
+    Collider2D wall;
+    
+    // Last Wall's End
+    Vector2 lastWallEnd;
+
+    // Use this for initialization
+    void Start () {
+        // Initial Velocity
+        GetComponent<Rigidbody2D>().velocity = Vector2.up * speed;
+        spawnWall();
+    }
+    
+    // Update is called once per frame
+    void Update () {
+        // Check for key presses
+        if (Input.GetKeyDown(upKey) && currentDirection != "down") {
+            GetComponent<Rigidbody2D>().velocity = Vector2.up * speed;
+            currentDirection = "up";
+            spawnWall();
+        }
+        else if (Input.GetKeyDown(downKey) && currentDirection != "up") {
+            GetComponent<Rigidbody2D>().velocity = -Vector2.up * speed;
+            currentDirection = "down";
+            spawnWall();
+        }
+        else if (Input.GetKeyDown(rightKey) && currentDirection != "left") {
+            GetComponent<Rigidbody2D>().velocity = Vector2.right * speed;
+            currentDirection = "right";
+            spawnWall();
+        }
+        else if (Input.GetKeyDown(leftKey) && currentDirection != "right") {
+            GetComponent<Rigidbody2D>().velocity = -Vector2.right * speed;
+            currentDirection = "left";
+            spawnWall();
+        }
+        
+        fitColliderBetween(wall, lastWallEnd, transform.position);
+    }  
+    
+    void spawnWall() {
+        // Save last wall's position
+        lastWallEnd = transform.position;
+        
+        // Spawn a new Lightwall
+        GameObject g = (GameObject)Instantiate(wallPrefab, transform.position, Quaternion.identity);
+        wall = g.GetComponent<Collider2D>();
+    }
+    
+    void fitColliderBetween(Collider2D co, Vector2 a, Vector2 b) {
+        // Calculate the Center Position
+        co.transform.position = a + (b - a) * 0.5f;
+        
+        // Scale it (horizontally or vertically)    
+        float dist = Vector2.Distance(a, b);
+        if (a.x != b.x)
+            co.transform.localScale = new Vector2(dist + 1, 1);
+        else
+            co.transform.localScale = new Vector2(1, dist + 1);
+    }
+    
+    void OnTriggerEnter2D(Collider2D co) {
+        // Not the current wall?
+        if (co != wall) {
+            print("Player lost:" + name);
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Bonus-GroundhogGame/Scripts/Mole.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Mole : MonoBehaviour
+{
+    // On-hit effect
+    private SpriteRenderer renderer;
+    // Animation controller
+    private Animator anim;
+
+
+    private void Awake()
+    {
+        // Get a reference to the SpriteRenderer component on this gameObject
+        renderer = GetComponent<SpriteRenderer>();
+        // Get a reference to the Animator component on this gameObject
+        anim = GetComponent<Animator>();
+
+    }
+
+    void OnMouseDown()
+    {
+        // If the game is not over
+        if (!Score.Instance.gameOver)
+        {
+            // Load on-hit effect
+            renderer.material.SetColor("_Color", Color.red);
+            anim.SetBool("isHit", true);
+            // Add one to the user's score
+            Score.Instance.IncrementScore();
+        }
+    }
+}
